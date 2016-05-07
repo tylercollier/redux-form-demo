72e429b50743078cb6d00fca5ae577f64916a8e6
Tyler Collier <git@tylercollier.com>
2016-05-07 15:52:34 -0700

    Fix bug where validation happened before state was set, due to not using setState callback

--- src/containers/WithoutReduxForm/WithoutReduxForm.js
+++ src/containers/WithoutReduxForm/WithoutReduxForm.js
@@ -7,10 +7,10 @@ export default class WithoutReduxForm extends Component {
       formValues: {
         firstName: {
           value: '',
-          touched: false,
-          errors: null
+          touched: false
         }
-      }
+      },
+      errors: {}
     };
   }
 
@@ -22,37 +22,42 @@ export default class WithoutReduxForm extends Component {
           touched: true
         }
       }
-    });
-    this.validate(inputName, event.target.value);
+    }, this.validate);
   }
 
-  validate(inputName, value) {
-    switch (inputName) {
-      case 'firstName':
-        if (!value) {
-          this.setState({
-            formValues: {
-              [inputName]: {
-                errors: 'First name is required'
+  validate() {
+    for (const inputName of Object.keys(this.state.formValues)) {
+      switch (inputName) {
+        case 'firstName':
+          if (this.state.formValues[inputName].value) {
+            this.setState({
+              errors: {
+                [inputName]: null
               }
-            }
-          });
-        }
-        break;
-      default:
-        throw new Error('Invalid inputName: ' + inputName);
+            });
+          } else {
+            this.setState({
+              errors: {
+                [inputName]: 'This field is required'
+              }
+            });
+          }
+          break;
+        default:
+          throw new Error('Invalid inputName: ' + inputName);
+      }
     }
   }
 
   render() {
-    const { formValues: { firstName } } = this.state;
+    const { formValues: { firstName }, errors } = this.state;
     return (
       <div className="container">
         <h1>Without Redux-Form</h1>
 
         <label htmlFor="firstName">First name</label>
         <input type="text" className="form-control" {...firstName} onChange={event => this.onChange(event, 'firstName')} onBlur={event => this.onChange(event, 'firstName')}/>
-        {firstName.errors && <div className="text-danger">{firstName.errors}</div>}
+        {errors.firstName && <div className="text-danger">{errors.firstName}</div>}
       </div>
     );
   }
