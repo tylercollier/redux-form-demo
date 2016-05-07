4c7db8d22928b1534c77259c85f4f79bd80545cd
Tyler Collier <git@tylercollier.com>
2016-05-07 16:30:24 -0700

    Add last name field; fix nested state issues

--- src/containers/WithoutReduxForm/WithoutReduxForm.js
+++ src/containers/WithoutReduxForm/WithoutReduxForm.js
@@ -8,6 +8,10 @@ export default class WithoutReduxForm extends Component {
         firstName: {
           value: '',
           touched: false
+        },
+        lastName: {
+          value: '',
+          touched: false
         }
       },
       errors: {}
@@ -15,33 +19,33 @@ export default class WithoutReduxForm extends Component {
   }
 
   onChange(event, inputName) {
-    this.setState({
-      formValues: {
-        [inputName]: {
-          value: event.target.value,
-          touched: true
-        }
-      }
-    }, this.validate);
+    const newState = Object.assign({}, this.state);
+    newState.formValues[inputName] = {
+      value: event.target.value,
+      touched: true
+    };
+    this.setState(newState, this.validate);
   }
 
   validate() {
+    const updateState = inputNameX => {
+      if (this.state.formValues[inputNameX].value) {
+        const newState = Object.assign({}, this.state);
+        newState.errors[inputNameX] = null;
+        this.setState(newState);
+      } else {
+        const newState = Object.assign({}, this.state);
+        newState.errors[inputNameX] = 'This field is required';
+        this.setState(newState);
+      }
+    };
     for (const inputName of Object.keys(this.state.formValues)) {
       switch (inputName) {
         case 'firstName':
-          if (this.state.formValues[inputName].value) {
-            this.setState({
-              errors: {
-                [inputName]: null
-              }
-            });
-          } else {
-            this.setState({
-              errors: {
-                [inputName]: 'This field is required'
-              }
-            });
-          }
+          updateState(inputName);
+          break;
+        case 'lastName':
+          updateState(inputName);
           break;
         default:
           throw new Error('Invalid inputName: ' + inputName);
@@ -50,7 +54,7 @@ export default class WithoutReduxForm extends Component {
   }
 
   render() {
-    const { formValues: { firstName }, errors } = this.state;
+    const { formValues: { firstName, lastName }, errors } = this.state;
     return (
       <div className="container">
         <h1>Without Redux-Form</h1>
@@ -58,6 +62,9 @@ export default class WithoutReduxForm extends Component {
         <label htmlFor="firstName">First name</label>
         <input type="text" className="form-control" {...firstName} onChange={event => this.onChange(event, 'firstName')} onBlur={event => this.onChange(event, 'firstName')}/>
         {errors.firstName && <div className="text-danger">{errors.firstName}</div>}
+        <label htmlFor="lastName">Last name</label>
+        <input type="text" className="form-control" {...lastName} onChange={event => this.onChange(event, 'lastName')} onBlur={event => this.onChange(event, 'lastName')}/>
+        {errors.lastName && <div className="text-danger">{errors.lastName}</div>}
       </div>
     );
   }
