9041a1a56c26ab5ed1e1cf9b3f1fe46feef9724a
Tyler Collier <git@tylercollier.com>
2016-05-07 13:59:45 -0700

    Add error div to show first name is required

--- src/containers/WithoutReduxForm/WithoutReduxForm.js
+++ src/containers/WithoutReduxForm/WithoutReduxForm.js
@@ -1,31 +1,58 @@
 import React, {Component} from 'react';
 
 export default class WithoutReduxForm extends Component {
-  handleSubmit(event) {
-    console.log(arguments);
-    event.preventDefault();
+  constructor() {
+    super(arguments);
+    this.state = {
+      formValues: {
+        firstName: {
+          value: '',
+          touched: false,
+          errors: null
+        }
+      }
+    };
+  }
+
+  onChange(event, inputName) {
+    this.setState({
+      formValues: {
+        [inputName]: {
+          value: event.target.value,
+          touched: true
+        }
+      }
+    });
+    this.validate(inputName, event.target.value);
+  }
+
+  validate(inputName, value) {
+    switch (inputName) {
+      case 'firstName':
+        if (!value) {
+          this.setState({
+            formValues: {
+              [inputName]: {
+                errors: 'First name is required'
+              }
+            }
+          });
+        }
+        break;
+      default:
+        throw new Error('Invalid inputName: ' + inputName);
+    }
   }
 
   render() {
+    const { formValues: { firstName } } = this.state;
     return (
       <div className="container">
         <h1>Without Redux-Form</h1>
 
-        <form className="form-horizontal">
-          <div className="form-group">
-            <label htmlFor="firstName" className="col-sm-2">First name</label>
-            <div className={'col-sm-8 '}>
-              <input type="text" className="form-control"/>
-            </div>
-          </div>
-          <div className="form-group">
-            <div className="col-sm-offset-2 col-sm-10">
-              <button className="btn btn-success" onClick={this.handleSubmit}>
-                <i className="fa fa-paper-plane"/> Submit
-              </button>
-            </div>
-          </div>
-        </form>
+        <label htmlFor="firstName">First name</label>
+        <input type="text" className="form-control" {...firstName} onChange={event => this.onChange(event, 'firstName')} onBlur={event => this.onChange(event, 'firstName')}/>
+        {firstName.errors && <div className="text-danger">{firstName.errors}</div>}
       </div>
     );
   }
